5b8f510a1093e3e0b619ae0d501b78d3829a1206
seneca <senecamartin11@gmail.com>
2021-01-29 17:46:18 +0100

    Cambios de base de datos

--- main.py
+++ main.py
@@ -430,7 +430,7 @@ class MainApp(Tk):
     def __init__(self):
         Tk.__init__(self)
         self.geometry("920x600")
-        self.title("CRYPTO INVERSIONS")
+        self.title("INVERT IN CRYPTOS")
         self.resizable(0,0)
         self.simulador = Simulador(self)
         self.simulador.place(x=0, y=0)
